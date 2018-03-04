e507c99133935d148fdfe0977032db53094f9f99
RIchard Herald <ric.herald@gmail.com>
2018-03-04 15:00:12 -0300

    Nova arquitetura

--- Commons/src/main/java/br/com/sddbrc/commons/property/Property.java
+++ Commons/src/main/java/br/com/sddbrc/commons/property/Property.java
@@ -1,4 +1,4 @@
-package br.com.commons.property;
+package br.com.sddbrc.commons.property;
 
 import java.io.IOException;
 import java.io.InputStream;
---
+++ Security/src/main/java/br/com/sddbrc/security/Security.java
@@ -0,0 +1,5 @@
+package br.com.sddbrc.security;
+
+public interface Security {
+    public boolean basicSecurity(String username, String password);
+}
