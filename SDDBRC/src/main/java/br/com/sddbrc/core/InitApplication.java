5cdf7c8f3670d6deffe1cb58bfc515065d8a8531
RIchard Herald <richard.ferreira@itsolved.com.br>
2018-05-13 16:18:36 -0300

    Modificações na classe InitApplication
    Implementação de threads para os metodos StartConnection e SchedulerReplication
    criação de algumas constantes e variaveis.

--- SDDBRC/src/main/java/br/com/sddbrc/core/InitApplication.java
+++ SDDBRC/src/main/java/br/com/sddbrc/core/InitApplication.java
@@ -1,28 +1,60 @@
 package br.com.sddbrc.core;
 
 import br.com.sddbrc.configuration_impl.ConfigurationConnection_DBImpl;
+import br.com.sddbrc.configuration_impl.DatabaseRConfigurationImpl;
 import br.com.sddbrc.connection_impl.ConnectionImpl;
 import br.com.sddbrc.persistence_impl.PersistenceImpl;
 import br.com.sddbrc.replication.IReplication;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class InitApplication {
 
-    private final String clazz = "br.com.sddbrc.replicationImpl.ReplicationEasyImpl";
+    private final String classReplication = "br.com.sddbrc.replicationImpl.ReplicationPrimaryCopyAsynchronousImpl";
+    private final String classPersistence = "br.com.sddbrc.persistence_impl.PersistenceImpl";
     private PersistenceImpl persistence = new PersistenceImpl();
     private ConfigurationConnection_DBImpl configurationConnection = new ConfigurationConnection_DBImpl();
+    private DatabaseRConfigurationImpl databaseRConfiguration = new DatabaseRConfigurationImpl();
     private Runtime runtime = new Runtime();
     private ConnectionImpl connection = new ConnectionImpl();
     private static InitApplication initApplication = new InitApplication();
 
     public static void main(String[] args) throws Exception {
+        System.out.println("Ligando a plataforma");
         initApplication.startConfiguration();
         initApplication.startPersistence();
         initApplication.startReplication();
-        initApplication.startConnection();
+        initApplication.schedulerReplication();
+        initApplication.startServerSocket();
+        System.out.println("Plataforma ligada!!");
+    }
+    
+    public void schedulerReplication() throws Exception {
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    runtime.schedulerReplication();
+                } catch (Exception ex) {
+                    Logger.getLogger(InitApplication.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        t.start();
     }
 
-    public void startConnection() throws Exception {
-        connection.socket();
+    public void startServerSocket() throws Exception {
+         Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    connection.socket();
+                } catch (Exception ex) {
+                    Logger.getLogger(InitApplication.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        t.start();
     }
 
     public void startConfiguration() throws Exception {
@@ -30,10 +62,10 @@ public class InitApplication {
     }
 
     public void startReplication() throws Exception {
-        runtime.setReplicationClass((IReplication) Class.forName(clazz).newInstance());
+        runtime.setReplicationClass((IReplication) Class.forName(classReplication).newInstance());
     }
 
     public void startPersistence() throws Exception {
-        persistence.init(configurationConnection.getDatabasesWithConfiguration());
+        persistence.init(databaseRConfiguration.getDatabasesWithConfiguration(),classPersistence);
     }
 }
