3ccc070ab829b4a37badfbab48539c027bad6b9e
RIchard Herald <richard.ferreira@itsolved.com.br>
2018-05-16 21:39:57 -0300

    removendo o pool de conexão do teste

--- Teste/Teste/src/main/java/br/com/sddbrc/teste/JDBC.java
+++ Teste/Teste/src/main/java/br/com/sddbrc/teste/JDBC.java
@@ -5,7 +5,9 @@ import com.zaxxer.hikari.HikariDataSource;
 import java.io.IOException;
 import static java.lang.Thread.sleep;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Random;
 
@@ -15,16 +17,11 @@ public class JDBC {
     private static Random random = new Random(3000);
     private static final boolean continuar = true;
 
-    public JDBC(){
-        loadConfiguration();
-    }
-
     public void stressJDBCInsert(int quantidade) throws IOException, Exception {
         try {
             for (int i = 0; i < quantidade; i++) {
                 callJDBC();
             }
-            System.out.println("Finalizando o for --------------------");
         } catch (Exception e) {
             throw e;
         }
@@ -40,12 +37,12 @@ public class JDBC {
                     } catch (Exception e) {
                     }
                     while (continuar) {
+                        long ti = System.currentTimeMillis();
                         String script_pessoa = "INSERT INTO Pessoa (pessoa_Nome,pessoa_Cpf,pessoa_sexo,pessoa_dataNascimento,pessoa_Email) VALUES ('Richard','12345678900','M','12/05/2018 00:35:00.000','email@email.com')";
                         try (
-                             Connection conn = datasource.getConnection();
-                             PreparedStatement ps = conn.prepareStatement(script_pessoa, Statement.NO_GENERATED_KEYS);
+                                Connection conn = getConnection();
+                                PreparedStatement ps = conn.prepareStatement(script_pessoa, Statement.NO_GENERATED_KEYS);
                             ) {
-                            long ti = System.currentTimeMillis();
                             ps.executeUpdate();
                             long tf = System.currentTimeMillis();
                             long tempo = tf - ti;
@@ -61,22 +58,17 @@ public class JDBC {
         }
     }
 
-    public void loadConfiguration(){
+    public Connection getConnection() throws Exception {
         try {
-            HikariConfig hikariConfig = new HikariConfig();
-            hikariConfig.setPoolName("teste");
-            hikariConfig.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDataSource");
-            hikariConfig.setJdbcUrl("jdbc:sqlserver://192.168.0.107:1434;instanceName=AMBIENTE1;databaseName=Replication");
-            hikariConfig.setUsername("sa");
-            hikariConfig.setPassword("Islan@123");
-            hikariConfig.setMaximumPoolSize(500);
-            hikariConfig.addDataSourceProperty("cachePrepStmts", 100);
-            hikariConfig.addDataSourceProperty("prepStmtCacheSize", 2048);
-            hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", 4096);
-            datasource = new HikariDataSource(hikariConfig);
+            String driver = "com.microsoft.sqlserver.jdbc.SQLServerDataSource";
+            String url = "jdbc:sqlserver://192.168.0.107:1434;instanceName=AMBIENTE1;databaseName=Replication";
+            String user = "sa";
+            String pass = "Islan@123";
+            Class.forName(driver);
+            return DriverManager.getConnection(url, user, pass);
         } catch (NumberFormatException e) {
             throw e;
-        } catch (Exception e) {
+        } catch (ClassNotFoundException | SQLException e) {
             throw e;
         }
     }
